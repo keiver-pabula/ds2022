5ccd5524511a66ac204cc071bfe1d334be1dbb87
keiver-pabula <71681167+keiver-pabula@users.noreply.github.com>
2022-11-13 19:07:07 +0700

    Add files via upload

---
+++ helloworld/helloworld.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file helloworld.cpp
+ * @brief print "Hello World!"
+ * 
+ *
+ * @author 陈科辉Keiver.P (keiver3034@gmail.com)
+ * @date 13 NOV 2022
+ */
+
+#include<iostream>
+
+int main(int argc, char* argv[])
+{
+  /**
+  * @brief Print Hello World!
+  */
+  std::cout<<"Hello World!"<< std::endl;
+  return 0;
+}
